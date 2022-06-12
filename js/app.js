181ecfacfb46d0c442cbc7df72c48e0b88e2559a
Alper <alperuluses35@gmail.com>
2022-06-12 17:48:12 +0300

    magic scroll with some animations

--- js/app.js
+++ js/app.js
@@ -1,18 +1,15 @@
 const steps = document.querySelectorAll(".step");
 const options = {
   rootMargin: "0px",
-  threshold: 0.1,
+  threshold: 0.01,
 };
 
 let callback = (e) => {
-  e.forEach((v) => {
-    if (v.isIntersecting) {
-      console.log(e);
-      v.target.classList.add("active-step");
-    } else {
-      v.target.classList.remove("active-step");
-    }
-  });
+  for (const entry of e) {
+    console.log(entry);
+    entry.target.classList.toggle("active-step", entry.isIntersecting);
+    entry.target.children[0].classList.toggle("animate__backInRight", entry.isIntersecting);
+  }
 };
 
 const observer = new IntersectionObserver(callback, options);
@@ -20,28 +17,28 @@ const observer = new IntersectionObserver(callback, options);
 steps.forEach((v, i) => {
   observer.observe(v);
 });
-
+function nextStep(direction) {
+  let curretStep = document.querySelector(".active-step");
+  let currentIndex = parseInt(curretStep.getAttribute("scroll-step"));
+  let nextElement = document.querySelector(
+    '.step[scroll-step="' +
+      (direction == "down" ? currentIndex + 1 : currentIndex - 1) +
+      '"]'
+  );
+  if (nextElement) {
+    nextElement.scrollIntoView({
+      behavior: "smooth"
+    });
+  }
+}
 window.onwheel = (e) => {
-  let activity = true;
   if (e.deltaY >= 0) {
     // Scrolling Down with mouse
     console.log("Scroll Down");
-    let curretStep = document.querySelector(".active-step");
-    let currentIndex = curretStep.getAttribute("scroll-step");
-    document
-      .querySelector(
-        '.step[scroll-step="' + (parseInt(currentIndex) + 1) + '"]'
-      )
-      .scrollIntoView({ behavior: "smooth", block: "end" });
+    nextStep("down");
   } else {
     // Scrolling Up with mouse
-    console.log("Scroll Down");
-    let curretStep = document.querySelector(".active-step");
-    let currentIndex = curretStep.getAttribute("scroll-step");
-    document
-      .querySelector(
-        '.step[scroll-step="' + (parseInt(currentIndex) - 1) + '"]'
-      )
-      .scrollIntoView({ behavior: "smooth", block: "end" });
+    console.log("Scroll Up");
+    nextStep("up");
   }
 };
